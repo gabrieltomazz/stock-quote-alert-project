b99d2d10dfa0da7b7a56080681135f2e3cb04274
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2021-06-22 02:49:32 -0300

    Atualizado API, Incluindo Envio de Email

--- StockQuoteAlertProject/Program.cs
+++ StockQuoteAlertProject/Program.cs
@@ -4,6 +4,9 @@ using System.Net;
 using System.IO;
 using System.Text;
 using Newtonsoft.Json;
+using System.Net.Mail;
+using System.Threading;
+
 
 namespace StockQuoteAlertProject
 {
@@ -11,18 +14,46 @@ namespace StockQuoteAlertProject
     {
         public static void Main(string[] args)
         {
-            AVConnection connect1 = new AVConnection("it1lZ2rhzBCaMeV86Jfw4yFH");
-            StockData stock =  connect1.GetStockPrice("PETR3");
-            Console.WriteLine(stock.response[0].c);
+            // PETR4 28.95 26.90
+
+            // verify args
+            if (args.Length == 3)
+            {
+                AlertData alertData = new AlertData();
+                alertData.stock = args[0];
+                alertData.gain = Convert.ToDecimal(args[1]);
+                alertData.loss = Convert.ToDecimal(args[2]);
+
+                Console.WriteLine("Aplicação iniciada! ");
+                RunAlertStock alert = new RunAlertStock();
+                alert.run(alertData);
+
+            }
+            else
+            {
+                Console.WriteLine("Error! Fornece os parâmetros adequadamente! ");
+            }
         }
     }
 
+    // class to params 
+    public class AlertData
+    {
+
+        public string stock { get; set; }
+        public decimal gain { get; set; }
+        public decimal loss { get; set; }
+
+    }
+
+    // stock price from API
     public class ResponseStockAPI
     {
         // last price
-        public string c { get; set; }
+        public decimal c { get; set; }
     }
 
+    // Api Data
     public class StockData
     {
         public bool status { get; set; }
@@ -31,30 +62,101 @@ namespace StockQuoteAlertProject
         public List<ResponseStockAPI> response { get; set; }
     }
 
-    public class AVConnection
+    // connecting to API
+    public class FCSConnection
     {
         private readonly string _apiKey;
 
-        public AVConnection(string apiKey)
+        // constructor to API Key
+        public FCSConnection(string apiKey)
         {
             this._apiKey = apiKey;
         }
 
-        public StockData GetStockPrice(string stock)
+        // function to get StockData from FCS API
+        public decimal GetStockPrice(string stock)
         {
-            // https://fcsapi.com/api-v3/stock/latest?symbol=PETR3&exchange=Real-time%20derived,brazil&access_key=it1lZ2rhzBCaMeV86Jfw4yFH
+           
+            // API Request
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://" + $@"fcsapi.com/api-v3/stock/latest?symbol={stock}&exchange=Real-time%20derived,brazil&access_key={this._apiKey}");
             HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
 
-            Console.WriteLine(resp.GetResponseStream());
-
+            // convert JSson to a Object
             StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.UTF8);
             string results = sr.ReadToEnd();
-            StockData myDeserializedClass = JsonConvert.DeserializeObject<StockData>(results);
-            Console.WriteLine(myDeserializedClass);
+            StockData stockData = JsonConvert.DeserializeObject<StockData>(results);
             sr.Close();
 
-            return myDeserializedClass;
+            decimal stockPrice = stockData.code == 200 && stockData.response.Count > 0 ? stockData.response[0].c : 0;
+         
+            return stockPrice;
         }
     }
+
+    public class MailHelper
+    {
+        // public static IConfiguration AppSetting { get; }
+
+        public void MailSender(string content)
+        {
+            //var builder = new ConfigurationBuilder()
+            //.AddJsonFile("appsettings.json");
+            //var config = builder.Build();
+            // var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true).AddEnvironmentVariables().Build()
+
+            var client = new SmtpClient("smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("80ce30703e957b", "784f1f672a1977"),
+                EnableSsl = true
+            };
+            client.Send("no-reply@stockalert.com", "gabrieltomazlima1@gmail.com", "Stock Quote Alert", content);
+            Console.WriteLine("Alerta enviado!");
+        }
+
+    }
+
+    public class RunAlertStock
+    {
+        public void run(AlertData alertData)
+        {
+            // method once every 20 seconds.
+            Timer t = new Timer(TimerCallback, alertData, 1, 20000);
+            Console.ReadLine();
+
+        }
+
+        private static void TimerCallback(Object alertData)
+        {
+            // Call method to build alert
+            BuildAlert(alertData);
+
+            // Force a garbage collection to occur for this demo.
+            GC.Collect();
+        }
+
+        private static void BuildAlert(Object alert)
+        {
+            // cast object to AlertData
+            AlertData alertData = (AlertData)alert;
+
+            // init MailHelper
+            MailHelper mail = new MailHelper();
+            // init Api connection
+            FCSConnection connect1 = new FCSConnection("it1lZ2rhzBCaMeV86Jfw4yFH");
+            // Get stockData from API 
+            decimal priceStock = connect1.GetStockPrice(alertData.stock);
+
+            // verify alert
+            if (priceStock > alertData.gain)
+            {
+                mail.MailSender($@" Alerta! Ação {alertData.stock} está cotada a R${priceStock} - VENDA! ");
+            }
+            else if (priceStock < alertData.loss )
+            {
+                mail.MailSender($@" Alerta! Ação {alertData.stock} está cotada a R${priceStock} - COMPRA! ");
+            }
+
+        }
+    }
+
 }
